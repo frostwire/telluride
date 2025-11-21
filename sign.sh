cf477cca46c697235e4adeeec8fa368427fb90c8
Angel Leon <gubatron@gmail.com>
2025-11-21 11:47:53 -0700

    do not submit for notarization if you didn't sign

--- sign.sh
+++ sign.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 if [ $(uname -a | grep -c Darwin) == 0 ]
 then
   echo "telluride/sign.sh: this script is only meant for macos, exiting"
@@ -8,17 +10,35 @@ ARCH=`arch`
 if [ ${ARCH} == "i386" ]; then
   ARCH=x86_64
 fi
+
+echo "Signing telluride_macos.${ARCH}..."
 codesign --verbose=4 \
          -s KET68JTS3L \
          --entitlements Entitlements.plist \
          --options runtime \
          -f \
          telluride_macos.${ARCH}
+
+if [ $? -ne 0 ]; then
+  # If the signing key is not in this device, you need to get it from the private repo frostwire-tools/certs/Apple...
+  echo "telluride/sign.sh: ERROR - Failed to sign the binary"
+  exit 1
+fi
+
+echo "Verifying signature..."
 codesign -vvv telluride_macos.${ARCH}
+
+if [ $? -ne 0 ]; then
+  echo "telluride/sign.sh: ERROR - Signature verification failed"
+  exit 1
+fi
+
+echo "Successfully signed telluride_macos.${ARCH}"
 
 # This tool must be symlinked from our private repo given it includes credentials
 if [ -f ./notarizeMacOsApp.sh ]
 then
+  echo "Submitting for notarization..."
   ./notarizeMacOsApp.sh telluride_macos.${ARCH} com.frostwire.Telluride
 else
   echo "telluride/sign.sh: telluride_macos signed but not sent for notarization, notarizeMacOsApp.sh not found (symlink from private tools repository)"
